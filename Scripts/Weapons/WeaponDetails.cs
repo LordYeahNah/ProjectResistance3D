56aa4de4536a7a5ca34fb93de08aa48cf9343bae
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2024-04-24 22:38:44 +1000

    Created the initial class to store weapon details

---
+++ Scripts/Weapons/WeaponDetails.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using Godot;
+
+public enum EWeaponType
+{
+    WPN_Melee = 0,
+    WPN_Rifle = 1,
+    WPN_Shotgun = 2,
+    WPN_Pistol = 3,
+}
+
+public class WeaponDetails
+{
+    // === Weapon Details === //
+    // Store reference to the weapon ID
+    protected string _weaponID;
+    public string WeaponID => _weaponID;
+
+    public string WeaponName;                           // Name of the weapon     
+    public EWeaponType WeaponType;                          // Type of the weapon
+
+    // === Weapon Settings === //
+    protected float _damagePoints;
+    protected float _criticalHitChance;
+    protected float _criticalHitModifier;
+    protected float _weaponCooldown;                    // How long before the weapon can be used again
+
+    // === Ammo Settings === //
+    protected int _maxAmmoInMag;
+    public int MaxAmmoInMag => _maxAmmoInMag;
+
+
+    /// <summary>
+    /// Calculates the damage points
+    /// </summary>
+    /// <param name="modifier">Only modifier to apply</param>
+    /// <returns>Damage points for a hit</returns>
+    public float CalculateDamagePoints(float modifier)
+    {
+        float dp = _damagePoints * modifier;            // Create the initial damage points
+        // Create the number generator
+        RandomNumberGenerator rand = new RandomNumberGenerator();
+        rand.Randomize();
+
+        // Determine if a critical hit
+        bool isCritHit = rand.Randi() < _criticalHitChance;
+
+        // Apply damage modifier if it is a critical hit
+        if (isCritHit)
+            dp *= _criticalHitModifier;
+        return dp;
+    }
+
+    /// <summary>
+    /// Calculate damage points with multiple modifiers
+    /// </summary>
+    /// <param name="modifiers">List of modifiers to apply</param>
+    /// <returns>Damage points for a hit</returns>
+    public float CalculateDamagePoints(List<float> modifiers)
+    {
+        float dp = _damagePoints;
+        foreach (var m in modifiers)
+            dp *= m;
+
+        // Create the number generator
+        RandomNumberGenerator rand = new RandomNumberGenerator();
+        rand.Randomize();
+
+        // Determine if a critical hit
+        bool isCritHit = rand.Randi() < _criticalHitChance;
+
+        // Apply damage modifier if it is a critical hit
+        if (isCritHit)
+            dp *= _criticalHitModifier;
+
+        return dp;
+    }
+}
