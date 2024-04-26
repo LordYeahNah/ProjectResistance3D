a8949fab88d8b66abe79d46e638301d5ff9c1b7e
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2024-04-26 16:49:53 +1000

    Added updater to check if the character wants to shoot

---
+++ Scripts/StateMachine/Updaters/WantsToShootUpdater.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Godot;
+using NexusExtensions;
+
+public class WantsToShootUpdater : StateUpdater
+{
+    private CharacterController _ctrlRef;
+    public static readonly float CHANCE_TO_SHOOT_TIME = 0.4f;
+    RandomNumberGenerator _rand;
+
+    public WantsToShootUpdater(State stateRef, StateMachine stateMach) : base(stateRef, stateMach)
+    {
+        _rand = new RandomNumberGenerator();
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        if(_ctrlRef == null)
+        {
+            _ctrlRef = _stateMachine.Ctrl;
+            if (_ctrlRef == null)
+                GD.PrintErr("WantsToShootUpdater -> Failed to get reference to the character controller");
+        }
+    }
+
+    public override void OnUpdate(float dt)
+    {
+        base.OnUpdate(dt);
+
+        if(_ctrlRef != null && _ctrlRef.CanFire())
+        {
+            _rand.Randomize();
+            bool willShoot = _rand.Randf() < CHANCE_TO_SHOOT_TIME;
+            if (willShoot)
+            {
+                _stateMachine.SetStateProperty<bool>("WillShoot", true);
+            }
+        }
+    }
+}
