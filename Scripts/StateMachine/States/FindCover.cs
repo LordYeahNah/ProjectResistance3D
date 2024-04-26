fff150ba268d0eea949a0ada1fb0c73adac40a6e
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2024-04-26 16:49:18 +1000

    Added state to find and set the cover position

--- Scripts/StateMachine/States/FindCover.cs
+++ Scripts/StateMachine/States/FindCover.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Godot;
+using NexusExtensions;
 
-namespace ProjectResistance3D.Scripts.StateMachine.States
+public class FindCover : State
 {
-    internal class FindCover
+    public FindCover(StateMachine stateMach, bool hasExit = false, bool loop = false) : base(stateMach, hasExit, loop)
     {
+    }
+
+    public FindCover(StateMachine stateMach, SubStateMachine subState, bool hasExit = false, bool loop = false) : base(stateMach, subState, hasExit, loop)
+    {
+    }
+
+    public override void OnUpdate(float dt)
+    {
+        base.OnUpdate(dt);
+        
+        if(_stateMachine.Ctrl != null)
+        {
+            var coverPointCtrl = _stateMachine.Ctrl.FindNearestCoverPoint();
+            if(coverPointCtrl != null)
+            {
+                _stateMachine.SetStateProperty<Vector3>("MoveToLocation", coverPointCtrl.GetClosesCoverPointToSelf(_stateMachine.Ctrl.GlobalPosition));
+                _stateMachine.SetStateProperty<bool>("HasMoveToLocation", true);
+
+                OnFinish();
+            }
+        }
     }
 }
