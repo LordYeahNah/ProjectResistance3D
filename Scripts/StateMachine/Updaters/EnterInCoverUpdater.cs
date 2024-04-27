597c8964549479718d289666c6296479721d9fbd
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2024-04-27 11:05:50 +1000

    Created updater to enter the character into a cover position

---
+++ Scripts/StateMachine/Updaters/EnterInCoverUpdater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Godot;
+using NexusExtensions;
+
+public class EnterInCoverUpdater : State
+{
+    public EnterInCoverUpdater(StateMachine stateMach, bool hasExit = false, bool loop = false) : base(stateMach, hasExit, loop)
+    {
+    }
+
+    public EnterInCoverUpdater(StateMachine stateMach, SubStateMachine subState, bool hasExit = false, bool loop = false) : base(stateMach, subState, hasExit, loop)
+    {
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        if(_stateMachine != null)
+        {
+            if(!_stateMachine.GetStateProperty<bool>(StateMachineKeys.IS_AT_COVER_POSITION))
+            {
+                _stateMachine.SetStateProperty<bool>(StateMachineKeys.IS_AT_COVER_POSITION, true);
+                if (_stateMachine.Ctrl != null)
+                    _stateMachine.Ctrl.SetInCover(true);
+            }
+        }
+    }
+}
